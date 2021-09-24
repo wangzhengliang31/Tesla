da07ebadd1d65973aa5e6c4ba3469642b37f0ff8
wangzhengliang <wangzhengliang31@qq.com>
2021-09-24 17:31:49 +0800

    hbase init

--- HBase/src/main/java/sycn/HBaseToHive.java
+++ HBase/src/main/java/sycn/HBaseToHive.java
@@ -1,34 +1,66 @@
 package sycn;
 
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.Trash;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellScanner;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapred.TableMap;
+import org.apache.hadoop.hbase.mapred.TableMapReduceUtil;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.UUID;
+
 /**
  * @author wzl
- * @desc
+ * @desc HBase to Hive
  * @date 2021/9/22 6:42 下午
  **/
 public class HBaseToHive extends ToolRunner implements Tool {
-
     private static final Logger logger = LoggerFactory.getLogger(HBaseToHive.class);
 
-    // HBase
+    /**
+     * HBase
+     */
     private static final String HDFS_FS_DEFAULTFS = "fs.defaultFS";
     private static final String HDFS_FS_DEFAULTFS_VALUE = "hdfs://xxx:8020/";
     private static final String HBASE_ROOT_DIR = "hbase.rootdir";
     private static final String HBASE_ROOT_DIR_VALUE = "hdfs://xxxx:8020/hbase";
 
-    // Zookeeper
+    /**
+     * Zookeeper
+     */
     private static final String HBASE_NAME = "hbase.zookeeper.quorum";
     private static final String HBASE_VALUE = "xxxx1:2181;xxxx2:2181;xxxx3:2181";
 
-    // AppMaster
+    /**
+     * AppMaster
+     */
     private static final String YARN_APP_MAPREDUCE_AM_STAGING_DIR = "yarn.app.mapreduce.am.staging-dir";
 
-    // IO Serializations
+    /**
+     * IO Serializations
+     */
     private static final String HDFS_IO_SERIALIZATIONS = "io.serializations";
     private static final String HDFS_IO_SERIALIZATIONS_VALUE = "org.apache.hadoop.io.serializer.WritableSerialization" +
             ",org.apache.hadoop.io.serializer.avro.AvroSpecificSerialization" +
@@ -37,22 +69,213 @@ public class HBaseToHive extends ToolRunner implements Tool {
             ",org.apache.hadoop.hbase.mapreduce.ResultSerialization" +
             ",org.apache.hadoop.hbase.mapreduce.KeyValueSerialization";
 
-    //
+    /**
+     * Column Family
+     */
     private static final String HBASE_CF = "info";
+    /**
+     * StageDir
+     */
     private static final String STAGE_DIR_PREFIX = "hdfs://xxxx:8020/tmp/hbase/stagedir/";
+    /**
+     * TmpDir
+     */
+    private static final String TEP_DIR_PREFIX = "hdfs://xxxx:8020/tmp/hbase/region_tmp/";
+    /**
+     * Trash Stage Dir
+     */
+    private static final String STAGE_TRASH_DIR_PREFIX = "hdfs://xxxx:8020/user/hbase_tmp/.Trash/";
+
+    static class TableSnapshotMapper extends MapReduceBase implements TableMap<Text, NullWritable> {
+        private Text mkey = new Text();
+
+        @Override
+        public void map(ImmutableBytesWritable key, Result result, OutputCollector<Text, NullWritable> outputCollector,
+                        Reporter reporter) throws IOException {
+            StringBuffer stringBuffer = new StringBuffer();
+            CellScanner cellScanner = result.cellScanner();
+
+            SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+            boolean flag = false;
+            stringBuffer.append("7");
+
+            // rowkey CellScanner
+            while (cellScanner.advance()) {
+                Cell cell = cellScanner.current();
+                String qualifier = Bytes.toString(cell.getQualifierArray(), cell.getQualifierOffset(),
+                        cell.getQualifierLength());
+                String value = Bytes.toString(cell.getValueArray(), cell.getValueOffset(), cell.
+                        getValueLength());
+                try {
+                    if ("task_dt".equals(qualifier) && (format.parse(value).getTime() + 14 * 24 * 60 * 60 *
+                            1000) > System.currentTimeMillis()) {
+                        flag = true;
+                    }
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                if ("biz_type".equals(qualifier)) {
+                    continue;
+                }
+                stringBuffer.append("\u0001" + value);
+            }
+            if (flag && stringBuffer.toString().split("\u0001").length == 21) {
+                mkey.set(stringBuffer.toString());
+                outputCollector.collect(mkey, NullWritable.get());
+            }
+        }
+    }
 
     @Override
-    public int run(String[] strings) throws Exception {
-        return 0;
+    public int run(String[] args) throws Exception {
+        int result = -1;
+        Admin admin = null;
+        Connection connection = null;
+        String stagingDir = StringUtils.EMPTY;
+        String trashStagingDir = StringUtils.EMPTY;
+        FileSystem fs = null;
+        Configuration hbaseConfiguration = null;
+        String snapShotName = StringUtils.EMPTY;
+        try {
+            // ------------------------------------
+            // STEP 1. output_dir
+            //
+            // ------------------------------------
+            // ###
+            Configuration config = new Configuration();
+            setConf(config);
+            // ### output_dir
+            Path path = new Path(args[1]);
+            fs = path.getFileSystem(config);
+            // ###
+            if (fs.exists(path)) {
+                logger.info("[ScanSnapShotMR] - output_dir is exists");
+            }
+// ###
+            Trash trashTmp = new Trash(fs, config);
+           /* if (trashTmp.moveToTrash(path)) {
+                LOG.info("Moved to trash: " + path);
+            }*/
+            // ------------------------------------
+            // STEP 2. HbaseSnapShot
+            // ------------------------------------
+            hbaseConfiguration = HBaseConfiguration.create(config);
+            hbaseConfiguration.set(HDFS_IO_SERIALIZATIONS, HDFS_IO_SERIALIZATIONS_VALUE);
+            hbaseConfiguration.set(HBASE_ROOT_DIR, HBASE_ROOT_DIR_VALUE);
+            String tableName = args[0];
+            //
+            connection = ConnectionFactory.createConnection(hbaseConfiguration);
+            admin = connection.getAdmin();
+            if (admin == null) {
+                logger.error("[ScanSnapShotMR] - Hbase Admin Error");
+                return -1;
+            }
+//
+            boolean exists = admin.tableExists(TableName.valueOf(tableName));
+            if (!exists) {
+                logger.error("[ScanSnapShotMR] - Hbase tableName = " + tableName + " is not exists.");
+                return -1;
+            }
+// snapshot
+            snapShotName = tableName.split(":")[1] + "_snapshot" + UUID.randomUUID().toString().replace("-", "");
+//
+            logger.info("[ScanSnapShotMR] - snapShotName = " + snapShotName);
+            try {
+                admin.snapshot(snapShotName, TableName.valueOf(tableName));
+            } catch (Exception ex) {
+                logger.error("[ScanSnapShotMR] - CloneSnapShot Error", ex);
+                return -1;
+            }
+// ------------------------------------
+// STEP 3. HbaseSnapShot
+// ------------------------------------
+            stagingDir = STAGE_DIR_PREFIX + UUID.randomUUID().toString() + "/";
+            trashStagingDir = STAGE_TRASH_DIR_PREFIX + stagingDir;
+            hbaseConfiguration.set(YARN_APP_MAPREDUCE_AM_STAGING_DIR, stagingDir);
+// ### job
+            JobConf jobConf = new JobConf(hbaseConfiguration);
+// ### SnapShot
+            Path tmpPath = new Path(TEP_DIR_PREFIX + tableName.replaceAll(":", "_") + UUID.randomUUID().
+                    toString() + "/");
+            Path outPutPath = new Path(args[1]);
+            // ### MR
+            jobConf.setMapperClass(TableSnapshotMapper.class);
+            jobConf.setMapOutputKeyClass(Text.class);
+            jobConf.setMapOutputValueClass(NullWritable.class);
+            jobConf.setJarByClass(HBaseToHive.class);
+            // ------------------------------------
+            // STEP 4. MR
+            // ------------------------------------
+            // ###
+            TableMapReduceUtil.initTableSnapshotMapJob(snapShotName,
+                    HBASE_CF, TableSnapshotMapper.class, Text.class,
+                    NullWritable.class, jobConf, false, tmpPath);
+            // ### outputformate
+            FileOutputFormat.setOutputPath(jobConf, outPutPath);
+            TableMapReduceUtil.addDependencyJars(jobConf);
+            // ### job
+            RunningJob job = JobClient.runJob(jobConf);
+            job.waitForCompletion();
+            // ### result
+            result = (job.isSuccessful() == true) ? 1 : 0;
+        } catch (Exception ex) {
+            logger.error("[ScanSnapShotMR] - ex = {}", ex);
+            result = -1;
+        } finally {
+            try {
+// ###
+                Path path = new Path(stagingDir);
+                fs = path.getFileSystem(hbaseConfiguration);
+                Trash trashTmp = new Trash(fs, hbaseConfiguration);
+                // ###
+                if (fs.exists(path)) {
+               /* if (trashTmp.moveToTrash(path)) {
+                    LOG.info("Moved StagingDir to trash: " + path);
+}*/
+                }
+                // ### snapshot
+                logger.info("[ScanSnapShotMR] - SnapShotName = " + snapShotName);
+                admin.deleteSnapshot(snapShotName);
+                // ###
+                admin.close();
+                connection.close();
+            } catch (Exception ex2) {
+                logger.error("[ScanSnapShotMR] - finally ex = {}", ex2);
+                result = -1;
+            }
+        }
+// ###
+        return result;
+
     }
 
     @Override
     public void setConf(Configuration configuration) {
-
+        configuration.set(HBASE_NAME, HBASE_VALUE);
+        configuration.set(HDFS_FS_DEFAULTFS, HDFS_FS_DEFAULTFS_VALUE);
     }
 
     @Override
     public Configuration getConf() {
         return null;
+    }
+
+    public static void main(final String[] args) throws Exception {
+        if (ArrayUtils.isEmpty(args)) {
+            logger.error("[ScanSnapShotMR] - args is not empty!");
+            System.exit(-1);
+        }
+
+        if (args.length < 2 || args.length > 2) {
+            logger.error("[ScanSnapShotMR] - args size must be 2 ; 1) Hbase Table Name 2) Hbase Table Output Dir");
+        }
+
+        for (int i = 0; i < args.length; i++) {
+            if (logger.isInfoEnabled()) {
+                logger.info("[ScanSnapShotMR] - arg(" + i + ")=" + args[i]);
+            }
+        }
+
+        ToolRunner.run(HBaseConfiguration.create(), new HBaseToHive(), args);
     }
 }
---
+++ Hive/src/main/java/sync/HiveToHBase.java
@@ -0,0 +1,192 @@
+package sync;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.HFileOutputFormat2;
+import org.apache.hadoop.hbase.mapreduce.LoadIncrementalHFiles;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.util.MapReduceExtendedCell;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.phoenix.schema.types.PInteger;
+import org.apache.phoenix.schema.types.PLong;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivilegedExceptionAction;
+
+/**
+ * @author wzl
+ * @desc
+ * @date 2021/9/24 5:21 下午
+ **/
+public class HiveToHBase {
+    // ### ZK
+    public static final String ZK = "xxx1:2181,xxx2:2181,xxx3:2181";
+    // ###
+    public static final String SPLIT_SYMBOL = "\001";
+    // ###
+    public static final String CF = "CF";
+    // ###
+    public static final Log LOG = LogFactory.getLog(HiveToHBase.class);
+    // ### Hive NULL
+    public static final String HIVE_NULL_CONSTANT = "\\N";
+    // ###
+    public static final String EXEC_USER = "hbase";
+
+    // ### MapClass
+    public static class MapClass extends Mapper<LongWritable, Text, ImmutableBytesWritable, Cell> {
+        // [0] - RowKey
+        private String rowKey;
+        // [1] - id
+        private String id;
+        // [2] -
+        private String assistantNumber;
+        // [3] -
+        // 2020-06-25 19:00:41
+        private String createTime;
+        // [4] - userId
+        private String recordType;
+        // [5] -
+        private String content;
+        // [6] - ID
+        private String targetUserId;
+        // [7] -
+        private String subclazzNumber;
+        // [8] -
+        private String assistantName;
+        // Hbase
+        private ImmutableBytesWritable outKey;
+        private Cell cell;
+        // phoenixLong
+        PLong phoenixLong;
+        PInteger pInteger;
+        // MD5
+        MessageDigest md;
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            super.setup(context);
+            try {
+                md = MessageDigest.getInstance("MD5");
+                phoenixLong = PLong.INSTANCE;
+                pInteger = PInteger.INSTANCE;
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private String md5RowKey(String rowKey) {
+            md.update(rowKey.getBytes());
+            byte b[] = md.digest();
+            int i;
+            StringBuffer buf = new StringBuffer("");
+            for (int offset = 0; offset < b.length; offset++) {
+                i = b[offset];
+                if (i < 0)
+                    i += 256;
+                if (i < 16)
+                    buf.append("0");
+                buf.append(Integer.toHexString(i));
+            }
+            return buf.toString().substring(8, 24);
+        }
+
+        public void map(LongWritable inKey, Text inValue, Context context) throws ClassCastException {
+            // ###
+            String[] infos = inValue.toString().split(SPLIT_SYMBOL);
+            try {
+                String msg = String.format("id=%s,rowkey=%s,assistantNumber=%s,createTime=%s,recordType=%s, content=%s,targetUserId=%s,subclazzNumber=%s,assistantName=%s",
+                        id, rowKey, assistantNumber, createTime, recordType, content, targetUserId, subclazzNumber,
+                        assistantName);
+                //
+                if (LOG.isInfoEnabled()) {
+                    LOG.info(msg);
+                }
+                // ###
+                if (!StringUtils.isBlank(id) && !StringUtils.equals(id, HIVE_NULL_CONSTANT)) {
+                    // rowkey
+                    rowKey = md5RowKey(id);
+                    // ### rowkey
+                    outKey = new ImmutableBytesWritable(Bytes.toBytes(rowKey));
+                    cell = new KeyValue(Bytes.toBytes(rowKey), Bytes.toBytes(CF), Bytes.toBytes("ID"),
+                            phoenixLong.toBytes(Long.parseLong(id)));
+                    context.write(outKey, new MapReduceExtendedCell(cell));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // ### Main
+    public static void main(String[] args) throws Exception {
+        // ###
+        Configuration conf = new Configuration();
+        conf.set("hbase.zookeeper.quorum", ZK);
+        final String[] paramArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        Job hfileJob = Job.getInstance(conf);
+        hfileJob.setJobName("HbaseBulkLoadDemoMR");
+        hfileJob.setJarByClass(HiveToHBase.class);
+        hfileJob.setMapperClass(MapClass.class);
+        hfileJob.setNumReduceTasks(0);
+        hfileJob.setOutputKeyClass(ImmutableBytesWritable.class);
+        hfileJob.setOutputKeyClass(HiveToHBase.class);
+
+
+        FileInputFormat.addInputPath(hfileJob, new Path(paramArgs[0]));
+        FileOutputFormat.setOutputPath(hfileJob, new Path(paramArgs[1]));
+// --------------------------------------------------
+// Hbase
+// --------------------------------------------------
+        final Configuration hbaseConfiguration = HBaseConfiguration.create(conf);
+        hbaseConfiguration.set("io.serializations", "org.apache.hadoop.io.serializer.WritableSerialization,org.apache.hadoop.io.serializer.avro.AvroSpecificSerialization, org.apache.hadoop.io.serializer.avro. AvroReflectSerialization, org.apache.hadoop.hbase.mapreduce.MutationSerialization, org.apache.hadoop.hbase. mapreduce.ResultSerialization, org.apache.hadoop.hbase.mapreduce.KeyValueSerialization");
+        final Connection connection = ConnectionFactory.createConnection(hbaseConfiguration);
+        final TableName tableName = TableName.valueOf(paramArgs[2]);
+        final Table clazzLessonTable = connection.getTable(tableName);
+        HFileOutputFormat2.configureIncrementalLoad(hfileJob, clazzLessonTable, connection.getRegionLocator
+                (tableName));
+        int result = hfileJob.waitForCompletion(true) ? 0 : 1;
+        // ###
+        UserGroupInformation ugi = UserGroupInformation.createRemoteUser(EXEC_USER);
+        try {
+            ugi.doAs(new PrivilegedExceptionAction<String>() {
+                public String run() throws Exception {
+                    Configuration conf2 = new Configuration();
+                    conf2.set("hbase.zookeeper.quorum", ZK);
+                    conf2.set("hadoop.security.authentication", "simple");
+                    Configuration hbaseConfiguration2 = HBaseConfiguration.create(conf2);
+                    hbaseConfiguration2.set("io.serializations", "org.apache.hadoop.io.serializer.WritableSerialization, org.apache.hadoop.io.serializer.avro.AvroSpecificSerialization, org.apache.hadoop.io. serializer.avro.AvroReflectSerialization, org.apache.hadoop.hbase.mapreduce.MutationSerialization, org.apache. hadoop.hbase.mapreduce.ResultSerialization, org.apache.hadoop.hbase.mapreduce.KeyValueSerialization");
+                    Connection connection2 = ConnectionFactory.createConnection(hbaseConfiguration2);
+                    Admin admin2 = connection2.getAdmin();
+                    TableName tableName2 = TableName.valueOf(paramArgs[2]);
+                    Table clazzLessonTable2 = connection.getTable(tableName);
+                    LoadIncrementalHFiles loader = new LoadIncrementalHFiles(hbaseConfiguration2);
+                    loader.doBulkLoad(new Path(paramArgs[1]), admin2, clazzLessonTable2, connection2.
+                            getRegionLocator(tableName2));
+                    return "null";
+                }
+            });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+// ###
+        System.exit(result);
+    }
+}
